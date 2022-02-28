3b52643ee1583a5866a30311c153a0927d797eba
Tushar Raj <tushar.raj@monsterindia.com>
2022-02-28 11:03:20 +0530

    CRUD routes basic

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.get('/api/goals', (req, res) => {
-    res.json({message: 'Get goals'})
-})
+// app.get('/api/goals', (req, res) => {
+//     res.status(200).json({message: 'Get goals'})
+// })
+
+app.use('/api/goals', require('./routes/goalRoutes'))
 
 app.listen(port, ()=> console.log(`Server started on port ${port}`))
